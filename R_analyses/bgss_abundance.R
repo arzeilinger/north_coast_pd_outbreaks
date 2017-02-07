17e687047d9e1d6cc247471164118d3db28ddd61
Adam Zeilinger <arzeilinger@gmail.com>
2017-02-07 14:52:20 -0800

    Calculated trap count summary and plotted timeseries for all vineyard sites

--- R_analyses/bgss_abundance.R
+++ R_analyses/bgss_abundance.R
@@ -2,7 +2,7 @@
 
 rm(list = ls())
 # Load packages
-my.packages <- c("xlsx", "tidyr", "dplyr", "data.table", "ggplot2", "lubridate")
+my.packages <- c("xlsx", "tidyr", "dplyr", "data.table", "ggplot2", "lubridate", "scales")
 lapply(my.packages, require, character.only = TRUE)
 
 # Import functions
@@ -49,3 +49,43 @@ for(i in grep("Trap_Count", names(sonomaData))){
 }
 head(sonomaData)
 str(sonomaData)
+## Save cleaned up data set
+saveRDS(sonomaData, file = "output/sonoma_bgss_abundance_data.rds")
+
+#### Remove columns for "Trap_up", "#_days_trap_up", and "Spittle_Bug"
+trapCounts <- sonomaData %>% dplyr::select(., -contains("Trap_Up"), -contains("trap_up"), -contains("Spittle"), -contains("RHSS"))
+# Remove other columns that provide litte information -- "Latitude", "Longitude", and "BGSS_Count"
+# Trap coordinates are in "Latitude_(N)" and "Longitude_(W)"
+trapCounts <- trapCounts %>% dplyr::select(., -Latitude, -Longitude, -contains("BGSS_Count"))
+
+#### Transform to long format:
+# All trap change dates in a single column
+# All trap counts in a single column
+trapCounts <- trapCounts %>% gather(., key = "date_num", value = "date", contains("Date")) %>% gather(., key = "trap_num", value = "trap_count", contains("Trap_Count"))
+# Remove NAs from date and trap_count columns
+trapCounts <- trapCounts %>% dplyr::filter(., !is.na(date) & !is.na(trap_count))
+head(trapCounts)
+str(trapCounts)
+
+
+trapSummary <- trapCounts %>% group_by(vysite, date_num) %>% summarise(meanDate = mean(date), meanCount = mean(trap_count))
+
+#### Make some figures
+
+# Simple time-series plot with each vineyard site as a line
+timeSeriesPlot <- ggplot(data=trapSummary, aes(x=meanDate, y=meanCount)) +
+  #geom_line(aes(colour=vysite), size=1.25) +
+  geom_point(aes(colour=vysite), size=2.5) +
+  #geom_errorbar(aes(ymax=meancfu+secfu, ymin=meancfu-secfu), width=0.2) +
+  scale_x_date(name = "Date collected", labels = date_format("%Y-%m-%d")) + 
+  scale_y_continuous(name = "Number of BGSS") +
+  # ylab("% insects on source plant") + 
+  # ylim(c(0,100)) +
+  # xlab("Weeks post inoculation") +
+  theme_bw(base_size=18) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        panel.background = element_blank()) 
+timeSeriesPlot
